7c45634feed9f2cca0ebde282edea61df442ccd1
Vitaly Turovsky <vital2580@icloud.com>
2021-07-14 01:20:47 +0300

    record note

--- source.ts
+++ source.ts
@@ -33,6 +33,7 @@ export const fabulousCreateComponent = (options?: Options1): CreateComponentRetu
 
 type MagicLevelType = "impeccable" | "groundbreaking";
 
+// display instead: 0 - 10
 export const createAnotherComponent = (level: MagicLevelType, gridPos: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10) => {}
 
 type MagicFunction = (
